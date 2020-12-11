b16c387728ae8a2518a48c88cbd385cb7d53a8f1
vignesh181 <vigneshvj321@gmail.com>
2020-12-11 17:44:15 +0530

    program

---
+++ Vigneshwaran_835334/Afternoon/Spring Hands_on/EmployeeExercise/src/main/java/employeeapplication/EmployeeExerciseApplication.java
@@ -0,0 +1,13 @@
+package employeeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeExerciseApplication.class, args);
+	}
+
+}
---
+++ Vigneshwaran_835334/Afternoon/Spring Hands_on/EmployeeExercise/src/test/java/employeeapplication/EmployeeExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package employeeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
